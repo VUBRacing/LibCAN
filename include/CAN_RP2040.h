d8fd5bcfe3babb038e3b00d4b465a901af108cf0
Ethanael202 <Ethanael.Materna@vub.be>
2026-02-05 11:45:43 +0100

    Add RP2040 libfonew feather

---
+++ include/CAN_RP2040.h
@@ -0,0 +1,18 @@
+#ifndef CAN_RP2040_H
+#define CAN_RP2040_H
+
+#include <Arduino.h>
+#include "CAN_Library.h"
+#include <Adafruit_MCP2515.h>
+
+class CANRP2040 : public CANLibrary {
+public:
+    bool init() override;
+    void send(Message message) override;
+    Message read() override;
+
+private:
+    Adafruit_MCP2515 mcp(19);
+};
+
+#endif
